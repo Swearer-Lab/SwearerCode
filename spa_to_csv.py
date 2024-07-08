aba273d50de5a121bc4638a28e3cd21c8b6cde9e
ItGlows <JeremyNorth2026@u.northwestern.edu>
2024-07-08 17:37:32 -0500

    General updates, new code for converting series into CSVs
    General updates, new code for converting series into CSVs

---
+++ spa_to_csv.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+.srs. to .csv Converter
+
+Loads from a folder with a split Omnic IR series and compiles it into a 
+.csv file with the wavenumber on the rows and the time in seconds on the columns
+Created on Mon Jul  8 13:59:34 2024
+
+@author: JAmie North
+"""
+
+import numpy as np
+import pandas as pd
+import spectrochempy as scp
+import os  
+    
+   
+    
+
+"edit the name of your file here!"
+name = 'FTIR_231027_PtAl2O310_95KBr_1%CO_500s_550_150m_49mW_2' # Base name for your series, before the number of the split file
+i = 0 
+i_str = f'{str(i):0>4}'
+
+while os.path.exists(name + i_str +'.spa'):
+    spec_slice = scp.read_omnic(name + i_str +'.spa')#Grab data
+    #Getting time information from the name of the spectra and converting it into an axis on which to stack the spectra
+    #Can currently accomodate s and minute timescales
+    spec_slice_name = spec_slice.name
+    timename = float(spec_slice.name.replace('Linked spectrum at ','').replace(' min.','').replace(' sec.',''))
+    if spec_slice_name[-4:] == "min.":
+        timecoord = scp.Coord(data = [timename*60],name = 'Time', units = 's')
+    elif spec_slice_name[-4:] == "sec.":
+        timecoord = scp.Coord(data = [timename],name = 'Time', units = 's')
+    spec_slice.y = timecoord
+    if i == 0:
+        spec_2D = spec_slice
+    else:
+        spec_2D = scp.concatenate(spec_2D,spec_slice,axis ='y')#Code for stacking the data
+    i += 1
+    i_str = f'{str(i):0>4}'
+    
+spec_2D.name = name
+
+csv = spec_2D.data
+df_csv = pd.DataFrame(csv)
+df_csv.to_csv(name + ".csv")
+
+    
